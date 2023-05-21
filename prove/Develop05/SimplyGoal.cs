90a7e0067291010ada6779191da5e53e9e308fcb
KBearmann <114293395+KBearmann@users.noreply.github.com>
2023-05-20 18:56:54 -0700

    Create SimplyGoal.cs

---
+++ prove/Develop05/SimplyGoal.cs
@@ -0,0 +1,32 @@
+using System;
+
+class SimpleGoal : Goal
+{
+    
+    public SimpleGoal(){}
+    public SimpleGoal(string nameOfGoal, string description, int pointsToEarn):base(nameOfGoal, description, pointsToEarn){SetTypeOfGoal("SimpleGoal");}
+    
+    public override void ShowGoal()
+    {
+        if(GetStatusOfGoal()){SetStatusOfGoalPrint("X");}
+        else{ SetStatusOfGoalPrint(" ");}
+        Console.WriteLine($"[{GetStatusOfGoalPrint()}] {GetNameOfGoal()} ({GetDescription()})");
+    }
+    
+    public override void RecordEvent()
+    {
+        SetStatusOfGoal(true);
+        SetTotalPoints(GetPointsToEarn());
+    }
+    
+    public override string GenerateSaveString()
+    {
+        SetSaveString( $"{GetTypeOfGoal()}:{GetNameOfGoal()}:{GetDescription()}:{GetPointsToEarn()}:{GetStatusOfGoal()}");
+        return GetSaveString();
+    }
+
+ 
+      
+  
+    
+}
