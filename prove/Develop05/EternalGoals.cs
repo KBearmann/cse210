23dd9863439739da0217c3006e6b3f7fd881b865
KBearmann <114293395+KBearmann@users.noreply.github.com>
2023-05-20 18:47:12 -0700

    Create EternalGoals.cs

---
+++ prove/Develop05/EternalGoals.cs
@@ -0,0 +1,29 @@
+using System;
+
+class EternalGoal : Goal
+{
+    public EternalGoal(string nameOfGoal, string description, int pointsToEarn):base(nameOfGoal, description,pointsToEarn){SetTypeOfGoal("EternalGoal");}
+    
+    public override void ShowGoal()
+    {
+        if(GetStatusOfGoal()){SetStatusOfGoalPrint("X");}
+        else{ SetStatusOfGoalPrint(" ");}
+        Console.WriteLine($"[{GetStatusOfGoalPrint()}] {GetNameOfGoal()} ({GetDescription()}) -- Times done: {GetTimesAcomplished()}");
+    }
+    
+    
+    public override void RecordEvent()
+    {
+        SetTimesAcomplishedValue(GetTimesAcomplished() + 1);
+        SetTotalPoints(GetPointsToEarn()*GetTimesAcomplished());
+    }
+    
+    
+    public override string GenerateSaveString()
+    {
+        SetSaveString( $"{GetTypeOfGoal()}:{GetNameOfGoal()}:{GetDescription()}:{GetPointsToEarn()}:{GetTimesAcomplished()}");
+        return GetSaveString();
+    }
+
+    
+}
