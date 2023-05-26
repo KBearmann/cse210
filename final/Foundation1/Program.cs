168721a100684fcfa32af0270f00a0ab0b187e6d
KBearmann <114293395+KBearmann@users.noreply.github.com>
2023-05-26 08:35:33 -0700

    Update Program.cs
    Added video comments

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -5,24 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.Clear();
-        Video video = new Video("Programming", "James Kt", "102 seconds");
+        Video video = new Video("Progaming SODA", "Ally Kt", "1223 seconds");
         video.VideoInfo();
-        video.AddComment("Bob", "AWesome");
-        video.AddComment("Martha", "USeful");
-        video.AddComment("Lisa", "BAD");
+        video.AddComment("Cerrie", "Loved the video!");
+        video.AddComment("Valerie", "Thank you this helped so much!");
+        video.AddComment("Kevin", "Didn't help or solve my problem.");
         video.Comments();
-        Video video2 = new Video("Building Stuff", "Chris", "20 seconds");
+        Video video2 = new Video("Kite Flying and You", "Kristie", "2023 seconds");
         video2.VideoInfo();
-        video2.AddComment("Ames", "Cool");
-        video2.AddComment("greg", "i love building");
-        video2.AddComment("hans", "building is awesome");
+        video2.AddComment("Aries", "Been waiting on a kite viedo.");
+        video2.AddComment("Kerin", "I think you attached that kite wrong.");
+        video2.AddComment("Bryan", "Could you please do a Video about kite flying with fiends?");
         video2.Comments();
-        Video video3 = new Video("Vase Tutorial", "Yanes", "1568 seconds");
+        Video video3 = new Video("Fly Tying Tutorial", "Flyfish Projects", "1568 seconds");
         video3.VideoInfo();
-        video3.AddComment("Hannah", "gonna use this later");
-        video3.AddComment("brock", "didnt' know you could do this");
-        video3.AddComment("Kaitlin", "so pretty");
-        video3.AddComment("Genifer", "original");
+        video3.AddComment("Han", "going to spin some of these up!");
+        video3.AddComment("Joe", "Loved your techinque");
+        video3.AddComment("Katie", "so pretty");
+        video3.AddComment("Rob P", "original");
         video3.Comments();
     }
 }
